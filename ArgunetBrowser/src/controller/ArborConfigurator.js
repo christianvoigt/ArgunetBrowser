ef80a4ebd5bc67b1e4ebc7d60a39e82aea5c9cc7
Christian Voigt <1pxsolidblack@googlemail.com>
2013-02-13 23:46:12 +0100

    configuration back to normal.

---
+++ ArgunetBrowser/src/controller/ArborConfigurator.js
@@ -0,0 +1,23 @@
+// namespace:
+this.argunet = this.argunet||{};
+
+(function() {	
+	var ArborConfigurator = function(arborParticleSystem,width,height) {
+		var sys = arborParticleSystem;
+		this.configurate = function(nrOfNodes){
+			//Repulsion
+			//var r = 800/nrOfNodes;
+			var r = 400;
+			//Stiffness: the attraction between connected nodes. It acts like a spring. A higher stiffness means a stronger spring.
+			//var s = 250 + 20 * nrOfNodes;
+			var s = 400;
+			//Friction
+			var f = 0.7;
+			
+			console.log("repulsion: "+r + " stiffness: " + s + " friction: " + f);
+			sys.parameters({repulsion:r, stiffness: s, friction: f, gravity:true});
+		};
+	};
+    argunet.ArborConfigurator = ArborConfigurator;
+}());
+
